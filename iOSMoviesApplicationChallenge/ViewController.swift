e46f235ff4a91631ce598e2decc2a5499974c7c0
OshibaM <mohammed.oshiba@talabat.com>
2023-11-02 15:31:13 +0200

    Initial Commit

---
+++ iOSMoviesApplicationChallenge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOSMoviesApplicationChallenge
+//
+//  Created by Mohamed Abdelhamid Mohamed Oshaiba on 02/11/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
